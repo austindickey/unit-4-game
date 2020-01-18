902ed4fab90be79309d2b6a639be80727afc862f
austindickey <austindickey@me.com>
2020-01-18 14:07:03 -0600

    initial commit is so massive because I did not have internet access while starting on this project

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var randNum = Math.random * 100
+var randCrystalNum = Math.random * 100
+var wins = 0
+var losses = 0
+
+$(document).ready(function() {
+
+    function startGame() {
+        $("#randNum").text(randNum)
+    }
+
+})
